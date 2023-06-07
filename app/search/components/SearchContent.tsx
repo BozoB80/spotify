c6b105ee1a27dd0b4c392fabf905a4faf77e6d5f
BozoB80 <bozo.bakula@gmail.com>
2023-06-07 12:42:59 +0200

    commit

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
 import { Song } from "@/types";
 
@@ -26,7 +27,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
               onClick={() => {}}
             />
           </div>
-          {/* add like button */}
+          <LikeButton songId={song.id} />
         </div>
       ))}      
     </div>
