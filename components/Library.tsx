08141ea087c07d647354b04bdfea08cd4bed2561
BozoB80 <bozo.bakula@gmail.com>
2023-06-08 13:11:09 +0200

    commit

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,6 +7,7 @@ import { useUser } from '@/hooks/useUser'
 import useUploadModal from '@/hooks/useUploadModal'
 import { Song } from '@/types'
 import MediaItem from './MediaItem'
+import useOnPlay from '@/hooks/useOnPlay'
 
 interface LibraryProps {
   songs: Song[]
@@ -16,6 +17,8 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal()
   const { user } = useUser()
   const uploadModal = useUploadModal()
+
+  const onPlay = useOnPlay(songs)
 
   const onClick = () => {
     if (!user) {
@@ -39,7 +42,7 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
           <MediaItem 
             key={item.id}
             data={item}
-            onClick={() => {}}
+            onClick={(id: string) => onPlay(id)}
           />
         ))}
       </div>
